44e33b74ecece5444fafa0c86cb2f161e69a6c5b
alex <alexang@agibusiness.com>
2024-09-04 07:05:38 -0700

    Fix a bug for validators of email

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
@@ -21,6 +21,13 @@ type PasswordFieldProps = {
   icon: string
 }
 
+enum LoginFormStatus {
+  BASE = 'base',
+  LOGGING_USER = 'logging-user',
+  ERROR = 'error',
+  SUCCESS = 'success'
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -37,13 +44,33 @@ type PasswordFieldProps = {
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   passwordFieldProps: PasswordFieldProps = passwordNotVisibleProps
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
   onPasswordVisibilityClick() {
     if (this.passwordFieldProps === passwordVisibleProps) {
       this.passwordFieldProps = passwordNotVisibleProps;
     } else {
       this.passwordFieldProps = passwordVisibleProps
+    }
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado', this.loginForm.value);
+    } else {
+      console.log('Formulario no válido');
     }
   }
 
