c47ff724950fe3cf7570e0efb09543078c1e8b0a
alex <alexang@agibusiness.com>
2024-09-04 06:08:01 -0700

    Add initial input components for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
