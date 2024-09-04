7bb9195335d48423f47c3abefd5a97772b9f9a59
alex <alexang@agibusiness.com>
2024-09-04 06:19:30 -0700

    Add password visibility button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,31 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     ReactiveFormsModule,
     FloatLabelModule,
-    ButtonModule
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  passwordVisible: boolean
 
+  onPasswordVisibilityClick() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  get passwordVisibilityIcon(): string {
+    if (this.passwordVisible) {
+      return 'eye-slash'
+    }
+    return 'eye'
+  }
+
+  get passwordInputType(): string {
+    if (this.passwordVisible) {
+      return 'text'
+    }
+    return 'password'
+  }
 }
