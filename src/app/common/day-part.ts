aa16442a10af0f63672e1c2da766fb1ab9c958a8
alex <alexang@agibusiness.com>
2024-09-04 11:21:44 -0700

    Add afternoon

--- src/app/common/day-part.ts
+++ src/app/common/day-part.ts
@@ -1,20 +1,31 @@
 export enum DayPart {
     DAY = 'day',
+    AFTERNOON = 'afternoon',
     NIGHT = 'night'
 }
 
 export function getDayPart(date: Date): DayPart {
     const hour = date.getHours();
-    if (hour >= 6 && hour < 18) {
+    if (hour >= 4 && hour < 12) {
         return DayPart.DAY;
-    } else {
-        return DayPart.NIGHT;
     }
+
+    if (hour >= 12 && hour < 19) {
+        return DayPart.AFTERNOON;
+    }
+
+    return DayPart.NIGHT;
+
 }
 
 export function getSalutation(dayPart: DayPart): string {
     if (dayPart === DayPart.DAY) {
         return 'Buenos días';
     }
+
+    if (dayPart === DayPart.AFTERNOON) {
+        return 'Buenas tardes';
+    }
+
     return 'Buenas noches';
 }
