6c572abd364b13680515cfba978a9c84bc30a34b
alex <alexang@agibusiness.com>
2024-09-05 05:20:40 -0700

    Add sort dropdown

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -8,6 +8,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { DropdownModule } from 'primeng/dropdown';
+
+type SortOption = {
+  name: string,
+  key: string,
+}
 
 type LoadingFirstTime = {
   _type: 'loading-first-time';
@@ -35,6 +41,7 @@ type UsersStatus = LoadingFirstTime | LoadingSubsequentTime | BaseStatus;
     InputGroupModule,
     InputGroupAddonModule,
     ReactiveFormsModule,
+    DropdownModule,
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
@@ -43,7 +50,8 @@ export class UsersComponent {
 
   status: UsersStatus;
   searchControl: FormControl;
-  private debounceTimeout: any;
+  selectedSort: SortOption | undefined
+  debounceTimeout: any;
 
   constructor(
     private usersService: UsersService
@@ -68,6 +76,21 @@ export class UsersComponent {
       return [];
     }
     return this.status.response.items;
+  }
+
+  get sortOptions(): Array<SortOption> {
+    return [
+      { name: 'Nombre (Ascendente)', key: 'name-asc' },
+      { name: 'Nombre (Descendente)', key: 'name-desc' },
+      { name: 'Apellido (Ascendente)', key: 'last-name-asc' },
+      { name: 'Apellido (Descendente)', key: 'last-name-desc' },
+      { name: 'Rol (Ascendente)', key: 'role-asc' },
+      { name: 'Rol (Descendente)', key: 'role-desc' },
+      { name: 'Ciudad (Ascendente)', key: 'city-asc' },
+      { name: 'Ciudad (Descendente)', key: 'city-desc' },
+      { name: 'Estado (Ascendente)', key: 'state-asc' },
+      { name: 'Estado (Descendente)', key: 'state-desc' },
+    ];
   }
 
   formatUserLocation(user: UserPreview): string {
