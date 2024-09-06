5df087c6d477cfc8081703c945550a3bd9c66838
alex <alexang@agibusiness.com>
2024-09-06 05:19:46 -0700

    Add backend. Move frontend .gitignore to frontend

---
+++ backend/src/main/java/com/aram/erpcrud/ErpcrudApplication.java
@@ -0,0 +1,13 @@
+package com.aram.erpcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpcrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aram/erpcrud/ErpcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aram.erpcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
