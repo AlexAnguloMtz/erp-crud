9cd92795e2bc37e52b0b69e11a10804545eabed9
alex <alexang@agibusiness.com>
2024-10-09 10:15:21 -0700

    Add env files

---
+++ scripts/dev/clean-docker-compose.sh
@@ -0,0 +1,13 @@
+sudo docker container stop erp_backend
+
+sudo docker container rm erp_backend
+
+sudo docker container stop erp_database
+
+sudo docker container rm erp_database
+
+sudo docker container stop erp-crud-flyway-1
+
+sudo docker container rm erp-crud-flyway-1
+
+sudo docker volume rm erp-crud_erp_data
