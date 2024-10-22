cc539093815eb90775a6ad49be2d0f8411278041
alex <alexang@agibusiness.com>
2024-10-22 13:25:37 -0700

    Comment inventory module side bar link

--- frontend/src/app/pages/main-template/main-template.component.ts
+++ frontend/src/app/pages/main-template/main-template.component.ts
@@ -51,12 +51,12 @@ const links: Array<SidebarLink> = [
     text: 'Usuarios',
     icon: 'user',
   },
-  {
-    id: 'inventory',
-    href: '/home/inventory',
-    text: 'Inventario',
-    icon: 'box',
-  },
+  //{
+  //  id: 'inventory',
+  //  href: '/home/inventory',
+  //  text: 'Inventario',
+  //  icon: 'box',
+  //},
   //{
   //  id: 'movements',
   //  href: '/home/movements',
